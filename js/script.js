a2fe2d563775b91636e3de1cd6bd02bf76443167
Dendil <beylon228822@gmail.com>
2022-04-23 20:51:07 +0300

    2 exercise completed

--- js/script.js
+++ js/script.js
@@ -1,6 +1,17 @@
 "use strict";
+const questionNumber = 2;
 
-let numberOfFilms = prompt("Сколько фильмов вы уже посмотрели?", "");
+let numberOfFilms = +prompt("Сколько фильмов вы уже посмотрели?", "");
+
+if (numberOfFilms < 10) {
+    alert("Просмотрено довольно мало фильмов");
+} else if (numberOfFilms >= 10 && numberOfFilms < 30) {
+    alert("Вы классический зритель");
+} else if (numberOfFilms >= 30){
+    alert("Вы киноман");
+} else {
+    alert("Произошла ошибка");
+}
 
 const personalMovie = {
     count: numberOfFilms,
@@ -9,8 +20,18 @@ const personalMovie = {
     genres: [],
     privat: false
 };
-
-personalMovie.movies[prompt("Один из последних просмотренных фильмов?")] = prompt("На сколько оцените его?");
-personalMovie.movies[prompt("Один из последних просмотренных фильмов?")] = prompt("На сколько оцените его?");
+for (let i = 0; i < questionNumber; ++i) {
+    let firstAnswer = prompt("Один из последних просмотренных фильмов?");
+    if (firstAnswer == null || firstAnswer == '' || firstAnswer.length > 50) {
+        --i;
+        continue;
+    }
+    let secondAnswer = prompt("На сколько оцените его?");
+    if (secondAnswer == null || secondAnswer == '') {
+        --i;
+        continue;
+    }
+    personalMovie.movies[firstAnswer] = +secondAnswer;
+}
 
 console.log(personalMovie);
